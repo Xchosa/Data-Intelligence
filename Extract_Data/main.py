b682cc0d027a5acb0df73a09081701e2edb43515
Xchosa <pauloverbeck95@gmail.com>
2026-03-16 10:25:40 +0000

    error messags

--- Extract_Data/main.py
+++ Extract_Data/main.py
@@ -39,28 +39,28 @@ volume_name = "bronze_volume"
 
 recordLimit = 100
 offset =0
-
+save_on_Databricks=True
 mds_reference = ["countries", "cities", "airports","airlines", "aircrafts"]
 meta_data_key = ["CountryResource", "CityResource", "AirportResource","AirlineResource", "AircraftResource"]
 local_folder = ["Countries", "Cities", "Airports","Airlines", "Aircrafts"]
 
 #wworking
-#for ref, key, folder in zip(mds_reference, meta_data_key, local_folder):
-#    get_all_Recources.get_data_all_Reference(
-#        base_Url="https://lh-proxy.onrender.com",
-#        headers=headers,
+for ref, key, folder in zip(mds_reference, meta_data_key, local_folder):
+   get_all_Recources.get_data_all_Reference(
+       base_Url="https://lh-proxy.onrender.com",
+       headers=headers,
         
-#        catalog_name=catalog_name,
-#        schema_name=schema_name,
-#        volume_name=volume_name,
-#        mds_reference=ref,
-#        recordLimit=recordLimit,
-#        offset=offset,
+       catalog_name=catalog_name,
+       schema_name=schema_name,
+       volume_name=volume_name,
+       mds_reference=ref,
+       recordLimit=recordLimit,
+       offset=offset,
 
-#        save_on_Databricks=False,
-#        meta_data_key=key,
-#        local_folder=folder
-#    )
+       save_on_Databricks=save_on_Databricks,
+       meta_data_key=key,
+       local_folder=folder
+   )
 
 
 #one domestic,Eu, international
@@ -107,46 +107,46 @@ local_folder = ["Countries", "Cities", "Airports","Airlines", "Aircrafts"]
 #GET /operations/flightstatus/route/{origin}/{destination}/{date}?serviceType={serviceType}
 
 
-operations="operations"
-operation_type="flightstatus"
-operation_subtype="route"
-origin="FRA"
-destination="MUC"
-#todays date
-Date="2026-03-12"
-meta_data_key="ScheduleResource"
-recordLimit=20
-offset=0
-#Date=datetime.now().strftime("%Y-%m-%d")
+# operations="operations"
+# operation_type="flightstatus"
+# operation_subtype="route"
+# origin="FRA"
+# destination="MUC"
+# #todays date
+# Date="2026-03-12"
+# meta_data_key="ScheduleResource"
+# recordLimit=20
+# offset=0
+# #Date=datetime.now().strftime("%Y-%m-%d")
 
-serviceType="all"
+# serviceType="all"
 
 
-origin=["FRA", "FRA" , "FRA" "London", "NewYork"]
-destination =["MUC", "istanbul", "Bankok"]
+# origin=["FRA", "FRA" , "FRA" "London", "NewYork"]
+# destination =["MUC", "istanbul", "Bankok"]
 
-get_flight_route.get_data_flight_route(
-    base_Url="https://lh-proxy.onrender.com",
-    headers=headers,
+# get_flight_route.get_data_flight_route(
+#     base_Url="https://lh-proxy.onrender.com",
+#     headers=headers,
         
-    catalog_name=catalog_name,
-    schema_name=schema_name,
-    volume_name=volume_name,
+#     catalog_name=catalog_name,
+#     schema_name=schema_name,
+#     volume_name=volume_name,
 
-    operations= operations,
-    operation_type=operation_type,
+#     operations= operations,
+#     operation_type=operation_type,
 
-    recordLimit=recordLimit,
-    offset=offset,
+#     recordLimit=recordLimit,
+#     offset=offset,
 
-    save_on_Databricks=False,
-    local_folder=f"flight_{operation_type}",
+#     save_on_Databricks=False,
+#     local_folder=f"flight_{operation_type}",
     
-    meta_data_key=meta_data_key,
-	origin=origin,
-    destination=destination,
-    Date=Date,
+#     meta_data_key=meta_data_key,
+# 	origin=origin,
+#     destination=destination,
+#     Date=Date,
 	
-    serviceType=serviceTye
+#     serviceType=serviceTye
 
-    )
+#     )
