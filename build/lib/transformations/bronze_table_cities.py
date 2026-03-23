0aeeb682fdc1afe6994be84ef1b09f33a956d8e4
poverbec <paul.overbeck95@gmail.com>
2026-03-23 10:17:36 +0100

    resource pipeline

--- build/lib/transformations/bronze_table_cities.py
+++ build/lib/transformations/bronze_table_cities.py
@@ -1,15 +1,34 @@
 
 from pyspark import pipelines as dp
 from pyspark.sql.functions import col
-from pyspark.sql.functions import current_timestamp, input_file_name
+from pyspark.sql.functions import current_timestamp
 
-from src.Extract_Data.config import config
+
+
+import sys
+import os
 # This file defines a sample transformation.
 # Edit the sample below or add new transformations
 # using "+ Add" in the file browser.
 
-# @dp.table
-# def createTable():
+# Get the absolute path of the directory containing THIS script (src/ingestion)
+if "__file__" in globals():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+else:
+    current_dir = os.getcwd()
+
+# Go up one level to reach 'src'
+# This ensures that 'import utils.helpers' will work correctly
+src_root = os.path.abspath(os.path.join(current_dir, '..'))
+
+if src_root not in sys.path:
+    sys.path.append(src_root)
+
+# Now you can import your utils
+
+
+from Extract_Data.config import config
+
 path_cities = f"/Volumes/{config.catalog_name}/{config.schema_name}/{config.volume_name}/cities"
 #together a part of a pipile 
 @dp.table(
@@ -22,7 +41,7 @@ def cities_bronze():
         spark.readStream
             .format("cloudFiles")
             .option("cloudFiles.format", "json")
-            .load({path_cities})
+            .load(path_cities)
             .withColumn("_source_file", col("_metadata.file_path"))
             .withColumn("_ingested_at", current_timestamp())
             # .toTable("cities_bronze")
